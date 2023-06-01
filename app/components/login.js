82d400c702fd08a6f58c0858be35fac655e2ee68
Andy Zhou <zhoua2001@gmail.com>
2023-06-01 11:19:19 -0400

    changed login to dialogue popup

--- app/components/login.js
+++ app/components/login.js
@@ -2,6 +2,8 @@
 import Navbar from "./navbar";
 import { useState } from "react";
 
+import "./login.css"
+
 export default function Login(){
 	const [isOpen, setIsOpen] = useState(true);
 
@@ -13,19 +15,21 @@ export default function Login(){
 		<div className="login">
 			<button type="open" className="open-login-form" onClick={toggleForm}>Login</button>
 
-			{ isOpen && <div className="login-form" id="login-form">
-				<form>
-					<h1>Login</h1>
-					<label for="email">Email</label>
-					<input type="email" placeholder="Enter Email" className="login-form" name="email" required />
+			{ isOpen && 
+				<dialog open={isOpen}>
+					<form>
+						<h1>Login</h1>
+						<label for="email">Email</label>
+						<input type="email" placeholder="Enter Email" className="login-form" name="email" required />
 
-					<label for="password">Password</label>
-					<input type="password" placeholder="Enter Password" className="login-form" name="password" required />
+						<label for="password">Password</label>
+						<input type="password" placeholder="Enter Password" className="login-form" name="password" required />
 
-					<button type="submit" value="LOGIN" className="login-button">Submit</button>
-				</form>
-				<button type="close" className="close-form" onClick={toggleForm}>Close</button>
-			</div> }
+						<button type="submit" value="LOGIN" className="login-button">Submit</button>
+					</form>
+					<button type="close" className="close-form" onClick={toggleForm}>Close</button>
+				</dialog>
+			}	
 		</div>
 	)
 }
