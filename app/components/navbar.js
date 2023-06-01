32a7aa714d302cf5bc8ba42a5a4297f60a034daa
Andy Zhou <zhoua2001@gmail.com>
2023-06-01 17:11:25 -0400

    i'll make this look like not shit later

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -3,14 +3,28 @@ import Image from 'next/image';
 import Link from 'next/link';
 import Login from './login';
 
+import "./navbar.css";
+
 export default function Navbar() {
 	return (
-		<nav className="navbar" style={{padding: "0 5px 0 0"}}>
-			<Image src={catbot} alt="CatBot" width={75} height={100} style={{padding: "0 5px 0 0"}}/>
-			<Link href="/" style={{padding: "0 5px 0 0"}}>Home</Link>
-			<Link href="/champions" style={{padding: "0 5px 0 0"}}>Champions</Link>
-			<Link href="/about" style={{padding: "0 5px 0 0"}}>About</Link>
-			<Login />
+		<nav className="navbar">
+			{/* Navbar Logo */}
+			<Image className="navbar-logo" src={catbot} alt="CatBot" width={75} height={100} style={{padding: "0 5px 0 0"}}/>
+
+			{/* Navbar Links Menu */}
+			<ul className="navbar-list">
+				<input type="checkbox" id="navbar-toggle" className="navbar-toggle" />
+				<label htmlFor="navbar-toggle" className="navbar-toggle-label hamburger">&#9776;</label>
+
+				<div className="navbar-links">
+					<li><Link className="navbar-link" href="/">Home</Link></li>
+					<li><Link className="navbar-link" href="/champions">Champions</Link></li>
+					<li><Link className="navbar-link" href="/about">About</Link></li>
+				</div>
+				
+				<li><Login /></li>
+			</ul>
+			
 		</nav>
 	);
 }
