aef2c9c740d08b1071ee807c5de28334fbae6947
Andy Zhou <zhoua2001@gmail.com>
2023-06-01 12:15:42 -0400

    added click to close

--- app/components/login.js
+++ app/components/login.js
@@ -1,35 +1,52 @@
 'use client';
 import Navbar from "./navbar";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import "./login.css"
 
 export default function Login(){
-	const [isOpen, setIsOpen] = useState(true);
+	useEffect(() => {
+		const dialog = document.getElementById("login-form");
+		dialog.addEventListener("click", e => {
+			const dialogDimensions = dialog.getBoundingClientRect()
+			if (
+				e.clientX < dialogDimensions.left ||
+				e.clientX > dialogDimensions.right ||
+				e.clientY < dialogDimensions.top ||
+				e.clientY > dialogDimensions.bottom
+			) {
+				console.log("click outside");
+				dialog.close();
+			}
+		})
+	}, []);
 
-	function toggleForm() {
-		setIsOpen(!isOpen);
+	function closeLogin() {
+		document.getElementById("login-form").close();
+	}
+
+	function openLogin() {
+		document.getElementById("login-form").showModal();
 	}
 
 	return (
 		<div className="login">
-			<button type="open" className="open-login-form" onClick={toggleForm}>Login</button>
-
-			{ isOpen && 
-				<dialog open={isOpen}>
-					<form>
+			<button type="open" className="open-login-form" onClick={openLogin}>Login</button>
+			<div className="login-overlay">
+				<dialog id="login-form">
+					<form method="dialog">
 						<h1>Login</h1>
-						<label for="email">Email</label>
+						<label for="email">Email </label>
 						<input type="email" placeholder="Enter Email" className="login-form" name="email" required />
-
-						<label for="password">Password</label>
+						<br />
+						<label for="password">Password </label>
 						<input type="password" placeholder="Enter Password" className="login-form" name="password" required />
 
 						<button type="submit" value="LOGIN" className="login-button">Submit</button>
 					</form>
-					<button type="close" className="close-form" onClick={toggleForm}>Close</button>
+					<button type="close" className="close-form" onClick={closeLogin}>Close</button>
 				</dialog>
-			}	
+			</div>
 		</div>
 	)
 }
