c3c0164968cae7437213642c70dfb434ed453008
Andy Zhou <zhoua2001@gmail.com>
2023-06-02 01:40:20 -0400

    pretty sure i get how this works at the basics

---
+++ app/test/createtest.js
@@ -0,0 +1,42 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+export default function CreateTest() {
+    const [title, setTitle] = useState("");
+    const [text, setText] = useState("");
+
+    const router = useRouter();
+
+    const createTest = async () => {
+        const res = await fetch("http://127.0.0.1:8090/api/collections/test/records", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                title: title,
+                text: text,
+            })
+        });
+        const test = await res.json();
+        console.log(test);
+
+        setTitle("");
+        setText("");
+
+        router.refresh();
+    }
+
+    return (
+        <div>
+            <h1>Page</h1>
+            <form> 
+                <input type="text" placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)} />
+                <input type="text" placeholder="Text" value={text} onChange={(e) => setText(e.target.value)} />
+                <button type="button" onClick={createTest}>Create Test</button>
+            </form>
+        </div>
+    )
+}
--- app/test/page.js
+++ app/test/page.js
@@ -1,3 +1,5 @@
+import CreateTest from "./createtest";
+
 async function getTests() {
     const res = await fetch("http://127.0.0.1:8090/api/collections/test/records", {cache: "no-store"});
     const tests = await res.json();
@@ -5,13 +7,13 @@ async function getTests() {
 }
 
 export default async function Page() {
-
     const tests = await getTests();
 
     return (
         <div>
             <h1>Page</h1>
-            { tests.map((test) => (
+            <CreateTest />
+            { tests && tests.map((test) => (
                 <div key={test._id}>
                     <h2>{test.title}</h2>
                     <p>{test.text}</p>
