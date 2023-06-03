48d44b1d12bcb1aaf4dc1d4b103d7f0c6cab6562
Andy Zhou <zhoua2001@gmail.com>
2023-06-03 12:47:35 -0400

    removed pocketcontext

--- app/layout.js
+++ app/layout.js
@@ -1,19 +1,16 @@
 import Navbar from "@/app/components/navbar"
 import "./globals.css"
 import { UserProvider } from "@/app/context/usercontext"
-import { PocketProvider } from "./context/pocketcontext"
 
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
-			<PocketProvider>
-				<UserProvider>
-					<body>
-						<Navbar />
-						<div>{children}</div>
-					</body>
-				</UserProvider>
-			</PocketProvider>
+			<UserProvider>
+				<body>
+					<Navbar />
+					<div>{children}</div>
+				</body>
+			</UserProvider>
 		</html>
 	)
 }
