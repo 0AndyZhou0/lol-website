ad768a184c7d7e8f3b87c1ce26cc2fb7e417c823
Andy Zhou <zhoua2001@gmail.com>
2023-06-03 12:46:32 -0400

    sdk doesn't work for some reason

---
+++ app/test/showtests.js
@@ -0,0 +1,29 @@
+import RemoveTest from "./removetest";
+import PocketBase from 'pocketbase';
+
+
+
+export default async function Page() {
+    const getTests = async () => {
+        const res = await fetch("http://127.0.0.1:8090/api/collections/test/records");
+        const tests = await res.json();
+        console.log(tests.items);
+        return tests.items;
+    }
+
+    const tests = await getTests();
+
+    if (!tests) {
+        return <div>No tests</div>
+    }
+
+    return (
+        tests.map((test) => (
+            <div key={test.id}>
+                <h2>{test.title}</h2>
+                <p>{test.text}</p>
+                <RemoveTest id={test.id} />
+            </div>
+        ))
+    )
+}
