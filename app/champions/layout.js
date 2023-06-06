a4bae3484007a5d49b6b41100ebf2e9eb1906a8f
Andy Zhou <zhoua2001@gmail.com>
2023-06-05 20:20:37 -0400

    added page restrictions

--- app/champions/layout.js
+++ app/champions/layout.js
@@ -7,13 +7,25 @@ import "./layout.css"
 export default function Layout({ children }) {
     const [page, setPage] = useState(0);
 
+    function prevPage() {
+        if(page > 0){
+            setPage(page - 1);
+        }
+    }
+
+    function nextPage() {
+        if (page < 8) {
+            setPage(page + 1);
+        }
+    }
+
 	return (
         <div>
             {children}
             <div className="page-controller">
-                <button onClick={() => setPage(page - 1)}>Previous</button>
-                <h3>Page: {page}</h3>
-                <button onClick={() => setPage(page + 1)}>Next</button>
+                <button onClick={prevPage}>Previous</button>
+                <h3>Page: {page+1}</h3>
+                <button onClick={nextPage}>Next</button>
             </div>
             <LeagueChampions page={page} />
         </div>
